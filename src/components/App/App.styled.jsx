58a4bfba73c58bc31c29ada61b5038891a430b2f
OlgaTikhonchik <olga882011@gmail.com>
2023-05-29 00:54:23 +0300

    Update App.styled.jsx

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -1,1 +1,11 @@
 import styled from '@emotion/styled';
+
+export const AppContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  background-color: #815738;
+  margin: 0 auto;
+  padding: 20px;
+  max-width: 1200px;
+  flex-direction: column;
+`;
