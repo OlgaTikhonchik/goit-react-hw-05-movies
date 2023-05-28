6bbcbc60d3cc13a6aae29ff07b81374f58834003
OlgaTikhonchik <olga882011@gmail.com>
2023-05-28 12:59:01 +0300

    Create App.jsx

---
+++ src/components/App/App.jsx
@@ -0,0 +1,31 @@
+// import Home from 'pages/Home/Home';
+// import Movies from 'pages/Movies/Movies';
+import { Route, Routes } from 'react-router-dom';
+import SharedLayout from '../SharedLayout/SharedLayout';
+// import MovieDetails from 'pages/MovieDetails/MovieDetails';
+// import Cast from './Cast/Cast';
+// import Reviews from './Reviews/Reviews';
+import NotFound from '../../pages/NotFound/NotFound';
+import { lazy } from 'react';
+
+const Home = lazy(() => import('pages/Home/Home'));
+const Movies = lazy(() => import('pages/Movies/Movies'));
+const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
+const Cast = lazy(() => import('../Cast/Cast'));
+const Reviews = lazy(() => import('../Reviews/Reviews'));
+
+export const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
+};
