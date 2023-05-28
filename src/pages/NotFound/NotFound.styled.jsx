977065bcd2a3938902774658fb900e6de26a3829
OlgaTikhonchik <olga882011@gmail.com>
2023-05-29 00:53:50 +0300

    Update NotFound.styled.jsx

--- src/pages/NotFound/NotFound.styled.jsx
+++ src/pages/NotFound/NotFound.styled.jsx
@@ -1,1 +1,12 @@
 import styled from '@emotion/styled';
+
+export const TitleNotFound = styled.h1`
+  color: red;
+  text-align: center;
+`;
+
+export const TextNotFound = styled.p`
+  font-size: 18px;
+  color: red;
+  text-align: center;
+`;
