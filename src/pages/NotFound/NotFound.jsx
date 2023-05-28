e061d12197ff12339d4be2e2fd0839abcc9f8d2d
OlgaTikhonchik <olga882011@gmail.com>
2023-05-28 12:58:45 +0300

    Create NotFound.jsx

---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div>
+      <h1> Page not found </h1>
+      <p>Please, return to the home page. And try to enter a new movie</p>
+    </div>
+  );
+};
+
+export default NotFound;
